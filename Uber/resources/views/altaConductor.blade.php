746cac3b0f5576c3e0855e83cd9320252e6901cb
Alberto Luebbert <albertoluebbert@ideashappy.com>
2016-10-22 10:29:59 -0500

    Passwords

--- Uber/resources/views/altaConductor.blade.php
+++ Uber/resources/views/altaConductor.blade.php
@@ -2,8 +2,20 @@
 @section('title', 'Alta')
 @section('content')
 <h1>Uber:: Conductores</h1>
-			<a href="../conductores">Regresar a Conductores</a>
+			<!--a href="../conductores">Regresar a Conductores</a-->
 			{!!Form::open(['route'=>'conductores.store', 'method'=>'post'])!!}
+			
+			<div class="form-group">
+				{!!Form::label('Usuario(*):')!!}
+				{!!Form::text('usuarioC',null,['class'=>'form-control','placeHolder'=>'Ingresa tu Usuario'])!!}
+			</div>
+
+			<div class="form-group">
+				{!!Form::label('Contraseña(*):')!!}
+				{!!Form::password('passwordC',['class'=>'form-control','placeHolder'=>'Ingresa tu Contraseña'])!!}
+			</div>
+
+
 			<div class="form-group">
 				{!!Form::label('Nombre')!!}
 				{!!Form::text('nombreC',null,['class'=>'form-control','placeHolder'=>'Ingresa tu nombre'])!!}
@@ -19,25 +31,22 @@
 			</div>
 			<div class="form-group">
 				{!!Form::label('Selecciona Estado:')!!}
-			   <!--select class="form-control">
-					  <option>CDMX</option>
-					  <option>Edomex</option>
-				</select-->
-				<!--{!!Form::select('stateC',$estados)!!}-->
-				{{ Form::select('stateC', $estados, null, array('class' => 'form-control')) }}
-				
-				<!--{!!Form::select('size', array('L' => 'Large', 'S' => 'Small'))!!}-->
-				
+			    {{ Form::select('stateC', $estados, null, array('class' => 'form-control')) }}
 			</div>
+			
 			<div class="form-group">
 				{!!Form::label('Teléfono')!!}
 				{!!Form::text('telefonoC',null,['class'=>'form-control','placeHolder'=>'Ingresa tu telefono'])!!}
 			</div>
+			
 			<div class="form-group">
-				{!!Form::label('Selecciona la fotografia')!!}
-			    {!!Form::file('image')!!}
-			  </div>
-			  {!!Form::submit('Registrar Conductor',['class'=>'btn btn-primary'])!!}
+    		{!! Form::label('Fotografía') !!}
+    		{!! Form::file('image', null) !!}
+			</div>
+
+			
+
+			{!!Form::submit('Registrar Conductor',['class'=>'btn btn-primary'])!!}
 			
 			{!!Form::close()!!}
 @endsection
